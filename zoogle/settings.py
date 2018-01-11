7833f85b380946090607458aefb23d66027c00c8
blacksesion <blacksesion@gmail.com>
2018-01-11 16:03:49 -0300

    ajustes

--- zoogle/settings.py
+++ zoogle/settings.py
@@ -27,10 +27,8 @@ USER_AGENT_LIST = [
 
 DOWNLOADER_MIDDLEWARES = {
     'zoogle.middlewares.RandomUserAgentMiddleware': 400,
-    # 'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware': None,
-    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
-    # 'scrapy.contrib.spidermiddleware.referer.RefererMiddleware': True,
-    'scrapy.spidermiddlewares.referer.RefererMiddleware': True,
+    'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware': None,
+    'scrapy.contrib.spidermiddleware.referer.RefererMiddleware': True,
 }
 
 SPIDER_MODULES = ['zoogle.spiders']
