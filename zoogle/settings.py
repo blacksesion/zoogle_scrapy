b66104b4de05727a028f224bd22f1501933b6357
blacksesion <blacksesion@gmail.com>
2018-01-12 00:07:20 -0300

    ajustes

--- zoogle/settings.py
+++ zoogle/settings.py
@@ -16,9 +16,10 @@ HTTPERROR_ALLOW_ALL = True
 HTTPERROR_ALLOWED_CODES = [504]
 
 USER_AGENT_LIST = [
-    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.36 Safari/535.7',
-    'Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:16.0) Gecko/16.0 Firefox/16.0',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/534.55.3 (KHTML, like Gecko) Version/5.1.3 Safari/534.53.10'
+    #    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.36 Safari/535.7',
+    #    'Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:16.0) Gecko/16.0 Firefox/16.0',
+    #    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/534.55.3 (KHTML, like Gecko) Version/5.1.3 Safari/534.53.10',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'
 ]
 
 # DOWNLOADER_MIDDLEWARES = {'scrapy.contrib.spidermiddleware.httperror.HttpErrorMiddleware': True,}
@@ -31,10 +32,13 @@ DOWNLOADER_MIDDLEWARES = {
     'scrapy.contrib.spidermiddleware.referer.RefererMiddleware': True,
 }
 
+CONCURRENT_REQUESTS = 1
+DOWNLOAD_DELAY = 5
+
 SPIDER_MODULES = ['zoogle.spiders']
 NEWSPIDER_MODULE = 'zoogle.spiders'
 
-LOG_FILE = 'log_' + str(datetime.date.today()) + '.txt'
-LOG_ENABLED = True
-LOG_ENCODING = 'utf-8'
-ITEM_PIPELINES = {'zoogle.pipelines.SolrPipeline': 700}
+# LOG_FILE = 'log_' + str(datetime.date.today()) + '.txt'
+# LOG_ENABLED = True
+# LOG_ENCODING = 'utf-8'
+# ITEM_PIPELINES = {'zoogle.pipelines.SolrPipeline': 700}
