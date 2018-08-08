144638e574c03335eccc0addaa4b695eba7654b8
blacksesion <blacksesion@gmail.com>
2018-08-08 02:58:38 -0400

    local

--- zoogle/middlewares.py
+++ zoogle/middlewares.py
@@ -15,14 +15,15 @@ class RandomUserAgentMiddleware(object):
 class ProxyMiddleware(object):
     # overwrite process request
     def process_request(self, request, spider):
-        # Set the location of the proxy
-        # request.meta['proxy'] = "http://190.101.137.157:8080"
-        if settings.get('PROXY_POOL'):
-            request.meta['proxy'] = random.choice(settings.get('PROXY_POOL'))
+        if spider.name is not 'chileautos-lazy':
+            # Set the location of the proxy
+            # request.meta['proxy'] = "http://190.101.137.157:8080"
+            if settings.get('PROXY_POOL'):
+                request.meta['proxy'] = random.choice(settings.get('PROXY_POOL'))
 
-        # Use the following lines if your proxy requires authentication
-        proxy_user_pass = "USERNAME:PASSWORD"
-        # setup basic authentication for the proxy
-        encoded_user_pass = base64.encodestring(proxy_user_pass)
-        # request.headers['Proxy-Authorization'] = 'Basic ' + encoded_user_pass
-        print "\n Proxy usado: " + request.meta['proxy'] + "\n"
+            # Use the following lines if your proxy requires authentication
+            proxy_user_pass = "USERNAME:PASSWORD"
+            # setup basic authentication for the proxy
+            encoded_user_pass = base64.encodestring(proxy_user_pass)
+            # request.headers['Proxy-Authorization'] = 'Basic ' + encoded_user_pass
+            print "\n Proxy usado: " + request.meta['proxy'] + "\n"
